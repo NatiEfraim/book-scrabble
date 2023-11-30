fc6da3ee5e53049fad229e55a61b8c7dad817fef
NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>
2023-11-30 20:35:43 +0200

    done-bag-init-board
    Signed-off-by: NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>

--- EX1/test/Board.java
+++ EX1/test/Board.java
@@ -4,7 +4,6 @@ public class Board {
     private static final int BOARD_SIZE = 15;
     private static Board _mainBoard;
     public static Board getBoard() {
-        // Check if the board is not initialized
         if (_mainBoard == null) {
             // Create a new instance of the Board (calling the private constructor)
             new Board();
